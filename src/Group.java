13c11ddbb3feeb1ee9a55d653abba6d383b7d713
Egor Barkovsky <woldermind@gmail.com>
2021-02-08 20:56:12 +0300

    task complete .. . . L (

--- src/Group.java
+++ src/Group.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+//https://javarush.ru/groups/posts/431-10-voprosov-po-abstraktnihm-klassam-i-interfeysam-s-sobesedovaniy-po-jazihku-java
+// ЕСЛИ СПРАШИВАЮТ ПРО ПАТТЕРНЫ, НЕ НАЗЫВАЙТЕ СИНГЛТОН
 public class Group {
 
     private List<String> members;
---
+++ src/task/ListNode.java
@@ -0,0 +1,45 @@
+package task;
+
+
+//1 -> 2 -> 3 -> null
+//3 -> 2 -> 1 -> null
+public class ListNode {
+    Node head;
+
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        Node node1 = new Node(2);
+        Node node2 = new Node(3);
+    }
+
+    public static Node reverseNode(Node head) {
+        if (head == null) return null;
+
+        Node current = head; //1
+        Node previous = null; //0
+        Node nextNode = null; //0
+
+        while (current.nextNode != null) { //current.nextNode == 2, 3
+            nextNode = current.nextNode; //nextNode == 2, 3
+            current.nextNode = previous; //2.nextNode = 0, 1
+            previous = current; //previous = 1, 2
+            current = nextNode; //current = 2, 3
+        }
+        return null;
+    }
+}
+
+class Node {
+
+    int value;
+    Node nextNode;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+}
+
